5b76a86e4a0d2335780cb14f96d2cac80a1a2907
sandeep7032 <95558185+sandeep7032@users.noreply.github.com>
2022-11-10 20:48:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Heading from "./components/heading";
+import Text from "./components/text";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Text />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/heading.jsx
@@ -0,0 +1,10 @@
+import './universal.css';
+const heading = () => {
+    return (
+        <div>
+            <h1 className='centre-align heading'>About</h1>
+        </div>
+    );
+};
+
+export default heading;
